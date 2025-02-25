cefc890d227873cf62b97e1dabe1a23e03c5a967
freidylcarlos <freidylcarlos@gmail.com>
2025-02-24 19:39:06 -0500

    ** Initial Commit

---
+++ Source/Arkde_RoomPuzzle/Arkde_RoomPuzzleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Arkde_RoomPuzzleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARKDE_ROOMPUZZLE_API AArkde_RoomPuzzleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

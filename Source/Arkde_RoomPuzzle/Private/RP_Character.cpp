0640483a1019050b8dc130bd4e658ed3f7ea1e62
freidylcarlos <freidylcarlos@gmail.com>
2025-03-04 21:11:07 -0500

    ** General Animation

--- Source/Arkde_RoomPuzzle/Private/RP_Character.cpp
+++ Source/Arkde_RoomPuzzle/Private/RP_Character.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "RP_Character.h"
+# include "Camera/CameraComponent.h"
+
 
 // Sets default values
 ARP_Character::ARP_Character()
@@ -9,6 +10,9 @@ ARP_Character::ARP_Character()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	FPSCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FPS_CameraComponent"));
+	FPSCameraComponent->bUsePawnControlRotation = true;
+	FPSCameraComponent->SetupAttachment(RootComponent);
 }
 
 // Called when the game starts or when spawned
@@ -25,16 +29,6 @@ void ARP_Character::Tick(float DeltaTime)
 
 }
 
-// Called to bind functionality to input
-void ARP_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-	PlayerInputComponent->BindAxis("MoveForward", this, &ARP_Character::MoveForward);
-	PlayerInputComponent->BindAxis("MoveRight", this, &ARP_Character::MoveRight);
-
-}
-
 void ARP_Character::MoveForward(float value)
 {
 	AddMovementInput(GetActorForwardVector() * value);
@@ -45,3 +39,32 @@ void ARP_Character::MoveRight(float value)
 	AddMovementInput(GetActorRightVector() * value);
 }
 
+void ARP_Character::AddControllerPitchInput(float value)
+{
+	Super::AddControllerPitchInput(bIsLookInversion? -value : value);
+}
+
+void ARP_Character::Jump()
+{
+	Super::Jump();
+}
+
+void ARP_Character::StopJumping()
+{
+	Super::StopJumping();
+}
+
+// Called to bind functionality to input
+void ARP_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ARP_Character::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ARP_Character::MoveRight);
+
+	PlayerInputComponent->BindAxis("LookUp", this, &ARP_Character::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("LookRight", this, &ACharacter::AddControllerYawInput);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ARP_Character::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ARP_Character::StopJumping);
+}

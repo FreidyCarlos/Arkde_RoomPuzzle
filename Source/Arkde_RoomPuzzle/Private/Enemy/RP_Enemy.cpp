2efa50cabb62a5fa2200e7081f29669030d03c0c
freidylcarlos <freidylcarlos@gmail.com>
2025-06-05 23:46:59 -0500

    ** Patrol Config

---
+++ Source/Arkde_RoomPuzzle/Private/Enemy/RP_Enemy.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy/RP_Enemy.h"
+
+ARP_Enemy::ARP_Enemy()
+{
+	bLoopPath = false;
+	DirectionIndex = 1.0;
+	WaitingTimeOnPathPoint = 1.0f;
+}

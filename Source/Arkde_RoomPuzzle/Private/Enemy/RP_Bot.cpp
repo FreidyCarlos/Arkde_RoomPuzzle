a1959453ef2d7a04778ac48ed846d51166e2f8ea
freidylcarlos <freidylcarlos@gmail.com>
2025-06-12 23:21:32 -0500

    ** Bot Config

---
+++ Source/Arkde_RoomPuzzle/Private/Enemy/RP_Bot.cpp
@@ -0,0 +1,101 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy/RP_Bot.h"
+#include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "RP_Character.h"
+#include "NavigationSystem/Public/NavigationSystem.h"
+#include "NavigationSystem/Public/NavigationPath.h"
+#include "DrawDebugHelpers.h"
+#include "Components/RP_HealthComponent.h"
+#include "Materials/MaterialInstanceDynamic.h"
+
+// Sets default values
+ARP_Bot::ARP_Bot()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BotMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BotMeshComponent"));
+	BotMeshComponent->SetCanEverAffectNavigation(false);
+	BotMeshComponent->SetSimulatePhysics(true);
+	RootComponent = BotMeshComponent;
+
+	HealthComponent = CreateDefaultSubobject<URP_HealthComponent>(TEXT("HealthComponent"));
+
+	MinDistanceToTarget = 100.0f;
+	ForceMagnitude = 500.0f;
+}
+
+// Called when the game starts or when spawned
+void ARP_Bot::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	if (IsValid(PlayerPawn))
+	{
+		PlayerCharacter = Cast<ARP_Character>(PlayerPawn);
+	}
+
+	HealthComponent->OnHealthChangeDelegate.AddDynamic(this, &ARP_Bot::TakingDamage);
+
+	BotMaterial = BotMeshComponent->CreateAndSetMaterialInstanceDynamicFromMaterial(0, BotMeshComponent->GetMaterial(0));
+
+	NextPathPoint = GetNextPathPoint();
+}
+
+FVector ARP_Bot::GetNextPathPoint()
+{
+	if (!IsValid(PlayerCharacter))
+	{
+		return GetActorLocation();
+	}
+
+	UNavigationPath* NavigationPath = UNavigationSystemV1::FindPathToActorSynchronously(GetWorld(), GetActorLocation(), PlayerCharacter);
+	if (NavigationPath->PathPoints.Num() > 1)
+	{
+		return NavigationPath->PathPoints[1];
+	}
+
+	//if navigation points are less or equal than 1
+	return GetActorLocation();
+}
+
+void ARP_Bot::TakingDamage(URP_HealthComponent* CurrentHealthComponent, AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser)
+{
+	if (IsValid(BotMaterial))
+	{
+
+		BotMaterial->SetScalarParameterValue("Pulse", GetWorld()->TimeSeconds);
+	}
+
+	if (CurrentHealthComponent->IsDead())
+	{
+		Destroy();
+	}
+}
+
+// Called every frame
+void ARP_Bot::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	float DistanceToTarget = (GetActorLocation() - NextPathPoint).Size();
+	if (DistanceToTarget <= MinDistanceToTarget)
+	{
+		NextPathPoint = GetNextPathPoint();
+
+	}
+	else {
+		FVector ForceDirection = NextPathPoint - GetActorLocation();
+		ForceDirection.Normalize();
+		ForceDirection *= ForceMagnitude;
+
+		BotMeshComponent->AddForce(ForceDirection, NAME_None, true);
+	}
+
+	DrawDebugSphere(GetWorld(), NextPathPoint, 30.0f, 15, FColor::Purple, false, 0.0f, 0, 1.0f);
+}
+
---
+++ Source/Arkde_RoomPuzzle/Public/Enemy/RP_Bot.h
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "RP_Bot.generated.h"
+
+class UStaticMeshComponent;
+class ARP_Character;
+class UMaterialInstanceDynamic;
+
+UCLASS()
+class ARKDE_ROOMPUZZLE_API ARP_Bot : public APawn
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* BotMeshComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	URP_HealthComponent* HealthComponent;
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Bot Movement")
+	float MinDistanceToTarget;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Bot Movement")
+	float ForceMagnitude;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Bot")
+	FVector NextPathPoint;
+
+	UPROPERTY(BlueprintReadOnly, Category = "References")
+	ARP_Character* PlayerCharacter;
+
+	UMaterialInstanceDynamic* BotMaterial; 
+
+public:
+	// Sets default values for this pawn's properties
+	ARP_Bot();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION(Category = "Navigation")
+	FVector GetNextPathPoint();
+
+	UFUNCTION()
+	void TakingDamage(URP_HealthComponent* CurrentHealthComponent, AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
